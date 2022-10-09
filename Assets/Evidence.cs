995e6df762edc9982ef5769790245becfc0301bb
Jimmy Connors <jjc9bb@virginia.edu>
2022-10-09 18:34:04 -0400

    made the UI exist for an inventory, also set up the testing scene

---
+++ Assets/Evidence.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evidence
+{
+    public string name;
+    public string description;
+    public Sprite picture;
+    public bool discovered;
+}
