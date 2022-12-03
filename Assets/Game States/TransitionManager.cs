9f8c3d1bf7679b321f0152b9269ee08fbcfab478
giaozi <gchozick@gmail.com>
2022-12-03 15:32:24 -0500

    Added objection shake with placeholder

--- Assets/Game States/TransitionManager.cs
+++ Assets/Game States/TransitionManager.cs
@@ -4,52 +4,79 @@ using UnityEngine;
 using Yarn.Unity;
 using Transitions;
 
-namespace Transitions{
-    public enum TransitionType
-    {
-        fadeOutIn,
-    }
+namespace Transitions
+{
 
     public class TransitionManager : MonoBehaviour
     {
-     #region Singleton
-    private static TransitionManager _transitionManager;
+        #region Singleton
+        private static TransitionManager _transitionManager;
 
-    public static TransitionManager Instance { get { return _transitionManager; } }
+        public static TransitionManager Instance { get { return _transitionManager; } }
 
 
-    private void Awake()
-    {
-        if (_transitionManager != null && _transitionManager != this)
+        private void Awake()
         {
-            Destroy(this.gameObject);
+            if (_transitionManager != null && _transitionManager != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _transitionManager = this;
+            }
         }
-        else
+
+        #endregion
+        //Holds animation commands that can be called in yarn
+        public CanvasGroup curtainCanvasGroup;
+        public RectTransform objectionImage;
+        public bool playingTransition { get; private set; } = false;
+        public IEnumerator PlayTransition(string transition)
         {
-            _transitionManager = this;
+            playingTransition = true;
+            if (transition == "fadeOutIn")
+            {
+                yield return StartCoroutine(FadeOutFadeIn());
+            }
+            else if (transition == "objectionShake")
+			{
+                yield return StartCoroutine(ObjectionShake());
+			}
+            playingTransition = false;
+
         }
-    }
 
-    #endregion 
-    //Holds animation commands that can be called in yarn
-    public CanvasGroup curtainCanvasGroup;
 
-    public void PlayTransition(string transition){
-        if (transition  == "fadeOutIn") {
-            StartCoroutine(FadeOutFadeIn());
+        IEnumerator FadeOutFadeIn()
+        {
+            Debug.Log("entering: ProgressManager.FadeOutFadeIn()");
+            yield return StartCoroutine(Effects.FadeAlpha(curtainCanvasGroup, 0, 1, 2));
+
+            yield return new WaitForSeconds(2);
+
+            yield return StartCoroutine(Effects.FadeAlpha(curtainCanvasGroup, 1, 0, 2));
         }
-    } 
-    
 
-    IEnumerator FadeOutFadeIn()
-    {
-        Debug.Log("entering: ProgressManager.FadeOutFadeIn()");
-        yield return StartCoroutine(Effects.FadeAlpha(curtainCanvasGroup, 0, 1, 2));
+        IEnumerator ObjectionShake()
+        {
+            Debug.Log("entering: ProgressManager.ObjectionShake()");
+            float shakeDuration = 3f;
+            float shakeMagnitude = 100f;
+            float shakeRate = 0.001f;
+            Vector3 initialPosition = objectionImage.localPosition;
+            objectionImage.gameObject.SetActive(true);
+            while (shakeDuration > 0)
+            {
+                objectionImage.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+                yield return new WaitForSeconds(shakeRate);
+                shakeDuration -= shakeRate;
+            }
+            transform.localPosition = initialPosition;
+            objectionImage.gameObject.SetActive(false);
 
-        yield return new WaitForSeconds(2);
+            yield return null;
+        }
 
-        yield return StartCoroutine(Effects.FadeAlpha(curtainCanvasGroup, 1, 0, 2));
     }
-}
-
 }
